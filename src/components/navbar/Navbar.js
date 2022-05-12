450561dfacee979be3ce9c5eef7dd0c69a25a2d6
Jayraj-parki <jayrajfd@gmail.com>
2022-05-13 01:24:33 +0530

    added navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import s from "./navbar.module.scss"
 export default function Navbar({handler}) {
     return (
         <nav className={s.navbar}>
-            <a href="/">Lgm Todo</a>
+            <a href="/">Student Enrollment Form</a>
         </nav>
     )
 }
